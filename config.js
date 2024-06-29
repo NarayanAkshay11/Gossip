22b0d802d7c321b7aab83939146354fdc22bcba9
Akshay Narayanan B <90465549+NarayanAkshay11@users.noreply.github.com>
2024-06-29 16:57:59 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjJdT_azhDvxE1q4pkCEG1dM-GsNKTHi4",
+    authDomain: "gossipgurl-9c077.firebaseapp.com",
+    projectId: "gossipgurl-9c077",
+    storageBucket: "gossipgurl-9c077.appspot.com",
+    messagingSenderId: "236525812834",
+    appId: "1:236525812834:web:be5f1c02b801e538ff653b",
+    measurementId: "G-0M6F5WWG6Z"
+};
